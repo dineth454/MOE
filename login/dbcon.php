01e5d84a8cc1e4aa278a988011dddddc50d9348e
dineth454 <dineth454@gmail.com>
2016-03-30 01:50:11 -0700

    added new login files

---
+++ login/dbcon.php
@@ -0,0 +1,29 @@
+<?php
+
+class DBCon{
+	var $host = "localhost";
+	var $user = "root";
+	var $pass = "";
+	var $db = "moe";
+	var $myconn;
+
+	function connection(){
+		$con = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
+        
+        if (!$con) {
+            die('Could not connect to database!');
+        } else {
+            $this->myconn = $con;
+            //echo 'Connection established!';
+        }
+
+        return $this->myconn;
+    }
+
+    function close() {
+        mysqli_close($myconn);
+        echo 'Connection closed!';
+    }
+
+} 
+?>
---
+++ login/teacher.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>i'm a teacher bro!!</h1><br><br>
+</body>
+</html> 
