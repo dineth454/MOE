45accbfa2b13343f2d9b63ff2627b9b03c580207
nipuna_ <nipuna.player@gmail.com>
2016-03-31 22:58:56 +0530

    add viewmap page

---
+++ interfaces/mapview.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Map View</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../assets/css/simple-sidebar.css" rel="stylesheet">
+    <link href="../assets/css/home.css" rel="stylesheet">
+    <link href="../assets/css/smallbox.css" rel="stylesheet">
+    <link href="../assets/css/footer.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#" style="height: 80px;">
+                        GTMS
+                    </a>
+                </li>
+                <li>
+                    <a href="interface_0.1.php">Home</a>
+                </li>
+                <li>
+                    <a href="#">Shortcuts</a>
+                </li>
+                <li>
+                    <a class="active" href="#">Map view</a>
+                </li>
+                
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+        <nav class="navbar navbar-default" style="height: 65px; border-radius:0px;">
+            <div class="col-md-3 pull-right" style="margin-top: 18px;">
+                Nipuna Jayaweera 
+            <div class="pull-right"><a >Sign out</a></div>
+            </div>
+
+        </nav>
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            
+            <div class="container-fluid">
+                
+
+
+
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+<!--footer-->
+    <footer>
+        <div class="footer">
+            <div class="col-md-6 col-md-offset-2">
+                <li class="about" >
+                    <ul>
+                        <li><a href="#" class="footer-icon" target="_blank">Contact us</a></li>
+                        <li><a href="#" class="footer-icon" target="_blank">About us</a></li>
+                        <li><a href="#" class="footer-icon" target="_blank">Home</a></li>
+                    </ul>
+                </li>
+            </div>
+            <div class="col-md-2 col-md-offset-2"> 
+                <div style="margin-top: 25px;">
+                <a href="#" style="margin-left: 50px;">Developer site</a>
+                <img class="pull-right" src="../images/visa.png" style="width: 30px;">
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- jQuery -->
+    <script src="../assets/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../assets/js/bootstrap.min.js"></script>
+
+    
+</body>
+
+</html>
