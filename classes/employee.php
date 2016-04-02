b7396594a5194c23e5a86c9931633219d672b055
KalingaYapa <kkyapa@gmail.com>
2016-04-02 18:31:33 +0530

    modified employee.php

--- classes/employee.php
+++ classes/employee.php
@@ -68,44 +68,36 @@ $mysqli = $db->connection();
 		
 		function addEmployee($nic,$roleType,$designation,$nameInitials,$fName,$empID,$email,$dob,$currentAddress,$gender,$marrigeState,$mobileNum,$provinceID,$zoneID,$schoolId,$subject){
 			global $mysqli;
-			
-			echo $nic .'</br>';
-			echo $roleType.'</br>';
-			echo $designation.'</br>';
-			
+			// add ministry officer into the system
 			if($designation == '1'){
 				$instituteType = 1;
-				echo 'kalinga</br>';
-				
-				//$query_for_insert_user_table = 'insert into user (nic,password,roleTypeID) values ($nic,$nic,$roleType)';
-				//$result = $mysqli->query($query_for_insert_user_table);
 				$query_for_get_institute_id = "select instituteID from institute where instituteTypeID = '".$instituteType."'";
 				$resultOfInstituteId = $mysqli->query($query_for_get_institute_id);
 				$instituteIDArray = mysqli_fetch_assoc($resultOfInstituteId);
 				$instituteID = $instituteIDArray["instituteID"];
-				echo $instituteID;
+				
 				
 				if($instituteID != 0){
 					
-					echo 'kalinga yapa </br>';
 					$query_for_insert_data_into_employee = "insert into employee (nic,instituteID,roleType,designationTypeID,nameWithInitials,fullName,employeementID,email,currentAddress,gender,marrigeState,mobileNum) values ('$nic',$instituteID,$roleType,$designation,'$nameInitials','$fName','$empID','$email','$currentAddress','$gender','$marrigeState','$mobileNum')";
-					echo 'after query </br>';
+					
 					$result1  = $mysqli->query($query_for_insert_data_into_employee);
 					
-					echo 'afetr inserted </br>';
 					if($result1 == 1) {
 						
 						$query_for_insert_ministryOffeicer_tabel = "insert into ministry_officer (nic) values('$nic')";
 					
 						$result2 = $mysqli->query($query_for_insert_ministryOffeicer_tabel);
 						
-						echo 'insert into ministry officer tabel' ;
 						
 						if($result2 == 1){
 							$query_for_insert_user_table = "insert into user (nic,password,roleTypeID) values ('$nic','$nic',$roleType)";
 							$result = $mysqli->query($query_for_insert_user_table);
 							
-							echo 'successfully inssertd';
+							echo '<script language="javascript">';
+							echo 'alert("Employee successfully registered as a MinistryOfficer!!!  Thank You.")';
+							echo '</script>';
+						
 							
 						}
 						
@@ -114,6 +106,41 @@ $mysqli = $db->connection();
 				}
 				
 					
+				
+				// add province officer into the system
+			}else if($designation == '2'){
+				
+				$instituteType = 2;
+				
+				$query_for_get_institute_id = "select instituteID from province_office where provinceID = '".$provinceID."'";
+				$resultOfInstituteId = $mysqli->query($query_for_get_institute_id);
+				$instituteIDArray = mysqli_fetch_assoc($resultOfInstituteId);
+				$instituteID = $instituteIDArray["instituteID"];
+				
+				if($instituteID != 0){
+					
+					$query_for_insert_data_into_employee = "insert into employee (nic,instituteID,roleType,designationTypeID,nameWithInitials,fullName,employeementID,email,currentAddress,gender,marrigeState,mobileNum) values ('$nic',$instituteID,$roleType,$designation,'$nameInitials','$fName','$empID','$email','$currentAddress','$gender','$marrigeState','$mobileNum')";
+					$result1  = $mysqli->query($query_for_insert_data_into_employee);
+					if($result1 == 1) {
+						
+						$query_for_insert_proviceOffeicer_tabel = "insert into province_officer (nic) values('$nic')";
+					
+						$result2 = $mysqli->query($query_for_insert_proviceOffeicer_tabel);
+						
+						if($result2 == 1){
+							$query_for_insert_user_table = "insert into user (nic,password,roleTypeID) values ('$nic','$nic',$roleType)";
+							$result = $mysqli->query($query_for_insert_user_table);
+							
+							echo '<script language="javascript">';
+							echo 'alert("Employee successfully registered as a ProvinceOfficer!!!  Thank You.")';
+							echo '</script>';
+						}
+					}
+				}
+				// add Zonal officer into the system
+			}else if($designation == '3'){
+				
+				
 				
 				
 			}
