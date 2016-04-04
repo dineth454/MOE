1f685476bfa4e459cbee43972073e6b639922207
KalingaYapa <kkyapa@gmail.com>
2016-04-04 21:25:59 +0530

    include SideBar.php

--- interfaces/viewEmployee.php
+++ interfaces/viewEmployee.php
@@ -26,88 +26,8 @@
 
         <div id="wrapper">
 
-            <!-- Sidebar -->
-            <div id="sidebar-wrapper">
-                <ul class="sidebar-nav">
-                    <li class="sidebar-brand">
-                        <a href="#" style="height: 80px;">
-                            GTMS
-                        </a>
-                    </li>
-                    <li>
-                        <a class="active" href="interface_0.1.php">Home</a>
-                    </li>
-                    <li>
-                        <a  data-toggle="collapse" href="#collapse1">Users</a>
-                    </li>
-                    <div id="collapse1" class="panel-collapse collapse">
-                        <li>
-                            <a class="active" href="addEmployee.php">Add User</a>
-                        </li>
-                        <li>
-                            <a class="active" href="viewEmployee.php">View User</a>
-                        </li>
-                        <li>
-                            <a class="active" href="#">Update User</a>
-                        </li>
-                        <li>
-                            <a class="active" href="#">Delete User</a>
-                        </li>
-                    </div>
-                    <li>
-                        <a  data-toggle="collapse" href="#collapse2">Institutes</a>
-                    </li>
+            <?php include 'sideBarAdmin.php' ?>
 
-                    <div id="collapse2" class="panel-collapse collapse">
-                        <li>
-                            <a class="active" href="#">Add Zonal</a>
-                        </li>
-                        <li>
-                            <a class="active" href="#">Add School</a>
-                        </li>
-
-                    </div>
-                    <li>
-                        <a  data-toggle="collapse" href="#collapse3">View</a>
-                    </li>
-
-                    <div id="collapse3" class="panel-collapse collapse">
-                        <li>
-                            <a class="active" href="#">Province</a>
-                        </li>
-                        <li>
-                            <a class="active" href="#">Zonal</a>
-                        </li>
-                        <li>
-                            <a class="active" href="#">School</a>
-                        </li>
-
-                    </div>
-                    <li>
-                        <a  data-toggle="collapse" href="#collapse4">Edit</a>
-                    </li>
-
-                    <div id="collapse4" class="panel-collapse collapse">
-                        <li>
-                            <a class="active" href="#">Province</a>
-                        </li>
-                        <li>
-                            <a class="active" href="#">Zonal</a>
-                        </li>
-                        <li>
-                            <a class="active" href="#">School</a>
-                        </li>
-
-                    </div>
-                    <li>
-                        <a  data-toggle="collapse" href="#collapse5">Search</a>
-                    </li>
-                    <li>
-                        <a href="mapview.php">Map View</a>
-                    </li>
-
-                </ul>
-            </div>
             <!-- /#sidebar-wrapper -->
             <nav class="navbar navbar-default" style="height: 65px; border-radius:0px;">
                 <div class="col-md-3 pull-right" style="margin-top: 18px;">
@@ -122,60 +42,6 @@
                 <div class="container-fluid">
 
                     <div class="col-lg-9 col-lg-offset-1">
-
-
-                        <?php
-                        require("../classes/Employee.php");
-                        $employee = new Employee();
-
-                        if (isset($_POST['submit'])) {
-                            $nic = $roleType = $designation = $nameInitials = $fName = $empID = $email = $dob = $currentAddress = $gender = $marrigeState = $mobileNum = "";
-                            $provinceID = $zoneID = $schoolId = $subjectID = "";
-                            $nic = $_POST['nic'];
-                            $roleType = $_POST['select_role'];
-                            $designation = $_POST['designation'];
-                            $nameInitials = $_POST['name'];
-                            $fName = $_POST['fname'];
-                            $empID = $_POST['eId'];
-                            $email = $_POST['email'];
-                            $dob = $_POST['dob'];
-                            $currentAddress = $_POST['address'];
-                            $gender = $_POST['gender'];
-                            $marrigeState = $_POST['marrrige'];
-                            $mobileNum = $_POST['mobileNm'];
-
-
-                            //echo $designation;
-
-                            if ($designation == '2') {
-                                $provinceID = $_POST['provinceID'];
-                            } else if ($designation == '3') {
-                                $provinceID = $_POST['provinceID'];
-                                $zoneID = $_POST['zonalID'];
-                            } else if ($designation == '4') {
-                                $provinceID = $_POST['provinceID'];
-                                $zoneID = $_POST['zonalID'];
-                                $schoolId = $_POST['schoolId'];
-                            } else if ($designation == '5') {
-                                $provinceID = $_POST['provinceID'];
-                                $zoneID = $_POST['zonalID'];
-                                $schoolId = $_POST['schoolId'];
-                                $subjectID = $_POST['subject'];
-                            } else {
-                                $designation = $_POST['designation'];
-                            }
-
-                            $result = $employee->addEmployee($nic, $roleType, $designation, $nameInitials, $fName, $empID, $email, $dob, $currentAddress, $gender, $marrigeState, $mobileNum, $provinceID, $zoneID, $schoolId, $subjectID);
-
-
-
-
-
-
-                            // echo $nic;
-                        }
-                        ?>
-
                         <div align="center" style="padding-bottom:10px;">
                             <h1>View Employee</h1>
                         </div>
@@ -199,25 +65,6 @@
                                                     <option value="3">zonal Officer</option>
                                                     <option value="4">principal</option>
                                                     <option value="5">teacher</option>
-
-                                                    <?php
-                                                    /*   //$sqlQuery = "select * from designation";
-
-                                                      //$designationResult = $mysqli->query($sqlQuery);
-
-                                                      $designationTypeResult = $employee->loadDesignation();
-
-                                                      if(mysqli_num_rows($designationTypeResult) > 0 ){
-                                                      while($row = mysqli_fetch_assoc($designationTypeResult)){
-                                                      echo '<br>';
-                                                      echo '<option value="'.$row['designationTypeID'].'" >'.$row['designation'].'</option>';
-                                                      }
-
-                                                      }
-                                                     */
-                                                    ?>
-
-
                                                 </select>
                                                 <!--<label id="errorMain" style="font-size:10px;"></label>-->
                                             </div>
@@ -258,19 +105,16 @@
                                         </div>
                                     </div>
 
-                                  <!--      <div class="row">
-                                        <div class="form-group col-lg-12 col-md-12 col-sm-12">
-                                            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-                                                <button type="submit" name="submit" id="submit" class="btn btn-primary">Submit</button>
-                                            </div>
-
-                                        </div> -->
-                                    </div>
-
+                                    <!--      <div class="row">
+                                          <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                                              <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+                                                  <button type="submit" name="submit" id="submit" class="btn btn-primary">Submit</button>
+                                              </div>
+  
+                                          </div> -->
                                 </div>
 
                             </div>
-
                         </form>
                     </div>
 
