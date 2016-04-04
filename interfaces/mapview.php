ad75042a32775b75b41205179bff8c872f849428
KalingaYapa <kkyapa@gmail.com>
2016-04-04 16:53:23 +0530

    change interfaces

--- interfaces/mapview.php
+++ interfaces/mapview.php
@@ -1,169 +1,169 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Map View</title>
+        <title>Map View</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap Core CSS -->
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="../assets/css/simple-sidebar.css" rel="stylesheet">
-    <link href="../assets/css/home.css" rel="stylesheet">
-    <link href="../assets/css/smallbox.css" rel="stylesheet">
-    <link href="../assets/css/footer.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="../assets/css/simple-sidebar.css" rel="stylesheet">
+        <link href="../assets/css/home.css" rel="stylesheet">
+        <link href="../assets/css/smallbox.css" rel="stylesheet">
+        <link href="../assets/css/footer.css" rel="stylesheet">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <div id="wrapper">
+        <div id="wrapper">
 
-        <!-- Sidebar -->
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#" style="height: 80px;">
-                        GTMS
-                    </a>
-                </li>
-                <li>
-                    <a class="active" href="#">Home</a>
-                </li>
-                <li>
-                    <a  data-toggle="collapse" href="#collapse1">Users</a>
-                </li>
-				<div id="collapse1" class="panel-collapse collapse">
-					<li>
-                    <a class="active" href="#">Add User</a>
-					</li>
-					<li>
-						<a class="active" href="#">View User</a>
-					</li>
-					<li>
-						<a class="active" href="#">Update User</a>
-					</li>
-					<li>
-						<a class="active" href="#">Delete User</a>
-					</li>
-				</div>
-				<li>
-                    <a  data-toggle="collapse" href="#collapse2">Institutes</a>
-                </li>
-				
-				<div id="collapse2" class="panel-collapse collapse">
-					<li>
-                    <a class="active" href="#">Add Zonal</a>
-					</li>
-					<li>
-						<a class="active" href="#">Add School</a>
-					</li>
-					
-				</div>
-				<li>
-                    <a  data-toggle="collapse" href="#collapse3">View</a>
-                </li>
-				
-				<div id="collapse3" class="panel-collapse collapse">
-					<li>
-                    <a class="active" href="#">Province</a>
-					</li>
-					<li>
-						<a class="active" href="#">Zonal</a>
-					</li>
-					<li>
-						<a class="active" href="#">School</a>
-					</li>
-					
-				</div>
-				<li>
-                    <a  data-toggle="collapse" href="#collapse4">Edit</a>
-                </li>
-				
-				<div id="collapse4" class="panel-collapse collapse">
-					<li>
-                    <a class="active" href="#">Province</a>
-					</li>
-					<li>
-						<a class="active" href="#">Zonal</a>
-					</li>
-					<li>
-						<a class="active" href="#">School</a>
-					</li>
-					
-				</div>
-				<li>
-                    <a  data-toggle="collapse" href="#collapse5">Search</a>
-                </li>
-                <li>
-                    <a href="mapview.php">Map View</a>
-                </li>
-                
-            </ul>
-        </div>
-        <!-- /#sidebar-wrapper -->
-        <nav class="navbar navbar-default" style="height: 65px; border-radius:0px;">
-            <div class="col-md-3 pull-right" style="margin-top: 18px;">
-                Nipuna Jayaweera 
-            <div class="pull-right"><a >Sign out</a></div>
-            </div>
+            <!-- Sidebar -->
+            <div id="sidebar-wrapper">
+                <ul class="sidebar-nav">
+                    <li class="sidebar-brand">
+                        <a href="#" style="height: 80px;">
+                            GTMS
+                        </a>
+                    </li>
+                    <li>
+                        <a class="active" href="interface_0.1.php">Home</a>
+                    </li>
+                    <li>
+                        <a  data-toggle="collapse" href="#collapse1">Users</a>
+                    </li>
+                    <div id="collapse1" class="panel-collapse collapse">
+                        <li>
+                            <a class="active" href="#">Add User</a>
+                        </li>
+                        <li>
+                            <a class="active" href="addEmployee.php">View User</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">Update User</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">Delete User</a>
+                        </li>
+                    </div>
+                    <li>
+                        <a  data-toggle="collapse" href="#collapse2">Institutes</a>
+                    </li>
 
-        </nav>
-        <!-- Page Content -->
-        <div id="page-content-wrapper" style="min-height: 550px;">
-            
-            <div class="container-fluid">
-                <div id="googleMap" style="width:500px;height:500px;"></div>
+                    <div id="collapse2" class="panel-collapse collapse">
+                        <li>
+                            <a class="active" href="#">Add Zonal</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">Add School</a>
+                        </li>
 
+                    </div>
+                    <li>
+                        <a  data-toggle="collapse" href="#collapse3">View</a>
+                    </li>
 
+                    <div id="collapse3" class="panel-collapse collapse">
+                        <li>
+                            <a class="active" href="#">Province</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">Zonal</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">School</a>
+                        </li>
 
-            </div>
-        </div>
-        <!-- /#page-content-wrapper -->
+                    </div>
+                    <li>
+                        <a  data-toggle="collapse" href="#collapse4">Edit</a>
+                    </li>
 
-    </div>
-    <!-- /#wrapper -->
+                    <div id="collapse4" class="panel-collapse collapse">
+                        <li>
+                            <a class="active" href="#">Province</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">Zonal</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">School</a>
+                        </li>
 
-<!--footer-->
-    <footer>
-        <div class="footer">
-            <div class="col-md-6 col-md-offset-2">
-                <ul class="footer-nav">
-                    <li><a href="AboutUs.aspx">About</a></li>
-                    <li><a href="Help.aspx">Help</a></li>
-                    <li><a href="AdminSitemap.aspx">Site map</a></li>
+                    </div>
+                    <li>
+                        <a  data-toggle="collapse" href="#collapse5">Search</a>
+                    </li>
+                    <li>
+                        <a href="mapview.php">Map View</a>
+                    </li>
+
                 </ul>
             </div>
-            <div class="col-md-2 col-md-offset-2"> 
-                <div>
-                <a href="#" style="margin-left: 50px;">Developer site</a>
-                <img class="pull-right" src="../images/visa.png" style="width: 30px;">
+            <!-- /#sidebar-wrapper -->
+            <nav class="navbar navbar-default" style="height: 65px; border-radius:0px;">
+                <div class="col-md-3 pull-right" style="margin-top: 18px;">
+                    Nipuna Jayaweera 
+                    <div class="pull-right"><a >Sign out</a></div>
+                </div>
+
+            </nav>
+            <!-- Page Content -->
+            <div id="page-content-wrapper" style="min-height: 550px;">
+
+                <div class="container-fluid">
+                    <div id="googleMap" style="width:500px;height:500px;"></div>
+
+
+
                 </div>
             </div>
+            <!-- /#page-content-wrapper -->
+
         </div>
-    </footer>
-    <!-- jQuery -->
-    <script src="../assets/js/jquery.js"></script>
+        <!-- /#wrapper -->
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../assets/js/bootstrap.min.js"></script>
+        <!--footer-->
+        <footer>
+            <div class="footer">
+                <div class="col-md-6 col-md-offset-2">
+                    <ul class="footer-nav">
+                        <li><a href="AboutUs.aspx">About</a></li>
+                        <li><a href="Help.aspx">Help</a></li>
+                        <li><a href="AdminSitemap.aspx">Site map</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-2 col-md-offset-2"> 
+                    <div>
+                        <a href="#" style="margin-left: 50px;">Developer site</a>
+                        <img class="pull-right" src="../images/visa.png" style="width: 30px;">
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- jQuery -->
+        <script src="../assets/js/jquery.js"></script>
 
-    <script src="../assets/js/googlemap.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../assets/js/bootstrap.min.js"></script>
 
-</body>
+        <script src="../assets/js/googlemap.js"></script>
+
+    </body>
 
 </html>
