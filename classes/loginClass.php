3542850e23c648de4ae1bc5041806440ba68a809
dineth454 <dineth454@gmail.com>
2016-04-09 17:29:10 +0530

    added setions to login

--- classes/loginClass.php
+++ classes/loginClass.php
@@ -8,30 +8,55 @@ require("dbcon.php");
 			$con = new DBCon();
 			$conn = $con->connection();
 			
-			//check username and password
+			//check nic and password
 			$query = "SELECT * FROM user WHERE nic = '".$nic."' AND password ='".$password."' LIMIT 1 ";
 			$result = mysqli_query($conn, $query);
 
+			//check password
 			$query2 = "SELECT nic FROM user WHERE nic = '".$nic."' LIMIT 1";
 			$result2 = mysqli_query($conn, $query2);
+
+			//get details from employee table
+			$query3 = "SELECT * FROM employee WHERE nic = '".$nic."' LIMIT 1";
+			$result3 = mysqli_query($conn, $query3);
 
 
 			if(mysqli_num_rows($result)==1) {
 				while($row = mysqli_fetch_assoc($result)){
-					$roleType = $row["roleTypeID"];
+					$roleTypeID = $row["roleTypeID"];
 					
 				}
+
+				if(mysqli_num_rows($result3)==1) {
+				while($row = mysqli_fetch_assoc($result3)){
+					$instituteID = $row["instituteID"];
+					$designationTypeID = $row["designationTypeID"];
+					$fullName = $row["fullName"];
+					
+					}
+				}
         		
-        		if($roleType==1){
+        		if($roleTypeID==1){
+        			session_start();
+					$_SESSION["roleTypeID"]= $roleTypeID;
+					$_SESSION["instituteID"]= $instituteID;
+					$_SESSION["designationTypeID"]= $designationTypeID;
+					$_SESSION["fullName"]= $fullName;
         			header("Location: admin.php"); /* Redirect browser */
 					exit();
         		}
-        		else if($roleType==2){
+        		else if($roleTypeID==2){
+        			session_start();
+					$_SESSION["roleTypeID"]= $roleTypeID;
+					$_SESSION["instituteID"]= $instituteID;
+					$_SESSION["designationTypeID"]= $designationTypeID;
+					$_SESSION["fullName"]= $fullName;
         			header("Location: teacher.php"); /* Redirect browser */
 					exit();
         		}
 
 			}else{
+				//if there is nic, password must not there. so it is a incorrect password
 				if(mysqli_num_rows($result2)==1){
 					echo "Check ur password again!";
 				}
--- interfaces/admin.php
+++ interfaces/admin.php
@@ -1,6 +1,15 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
 <h1>Hi i'm the ADMIN bro!!</h1><br><br>
+
+<?php
+echo "Favorite color is " . $_SESSION["roleTypeID"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["fullName"] . ".";
+?>
 </body>
 </html> 
