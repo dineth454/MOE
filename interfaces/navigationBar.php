298a60b2a5376c5dcfbe7da272cc421a286951cc
Pasii <pasindudeeyagahage@gmail.com>
2016-04-09 22:58:45 +0530

    add css

--- interfaces/navigationBar.php
+++ interfaces/navigationBar.php
@@ -1,57 +1,7 @@
-<style type="text/css">
-	
-
-.user-wrapper {
-    display: inline-block;
-    height: 32px;
-    margin: 2px 0;
-    text-align: right;
-    width: 256px;
-    vertical-align: top;
-}
-
-.user-links-wrapper {
-    display: inline-block;
-    margin-right: 16px;
-    width: 208px;
-}
-
-.user-links-row {
-    height: 16px;
-    line-height: 16px;
-    overflow: hidden;
-    white-space: nowrap;
-}
-
-.navbar {
-    -webkit-box-shadow: 0 10px 7px -8px #002E62;
-    -moz-box-shadow: 0 10px 7px -8px #002E62;
-    box-shadow: 0 10px 7px -8px #002E62;
-
-    /* the rest of your styling */
-}
-
-
-
-</style>
-
-<nav class="navbar navbar-default" style="height: 65px; border-radius:0px; background-color:#C0C0C0">
+<nav class="navbar navbar-default" style="height: 65px; border-radius:0px;">
                 <div class="col-md-3 pull-right" style="margin-top: 18px;">
-                	<div class="user-warpper">
-                		<div class="user-links-wrapper">
-                			<div class="user-link-row">
-                				Nipuna Jayaweera
-                			</div>
-                			<div class="user-links-row">
-                				<div><a href="../classes/signout.php">Sign out</a></div>
-                			</div>
-                			
-                		</div>
-
-
-                	</div>	 
-                     
-                    
+                    Nipuna Jayaweera 
+                    <div class="pull-right"><a href="../classes/signout.php">Sign out</a></div>
                 </div>
 
 </nav>
