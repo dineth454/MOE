a16084b9538b73a071850924f1690f5ebc37e973
dineth454 <dineth454@gmail.com>
2016-04-10 03:08:49 +0530

    complted 44% of login

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -1,10 +1,21 @@
 //nic validation for login
 function nicValidation() {
+	var pattern = /^[0-9]{9}(V|v){1}/;
+    //var nic = document.getElementById("nic").;
     var nic = document.forms["loginForm"]["nic"].value;
-    var nic = document.forms["loginForm"]["nic"].value;
-    
+
     if (nic == null || nic == "") {
-        alert("Form must be filled out");
+        document.getElementById("nic").focus();
+        document.getElementById("nic").style.borderColor = "red";
+        //document.getElementById("errorNicNumber").innerHTML = "required";
+                    //document.getElementById("errorNicNumber").style.color = "red";
+        return false;
+    }
+    else if ((pattern.test(nic)) == false || nic.length < 10) {
+        document.getElementById("nic").focus();
+        document.getElementById("nic").style.borderColor = "red";
+        //document.getElementById("errorNicNumber").innerHTML = "invalid type";
+        //document.getElementById("errorNicNumber").style.color = "red";
         return false;
     }
 }
