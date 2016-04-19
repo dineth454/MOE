8f53d3a1a92360a6de9c2895e246c8e6af540ebb
nipuna_ <nipuna.player@gmail.com>
2016-04-19 16:34:09 +0530

    edit addschool mapmarker

--- assets/js/googlemap.js
+++ assets/js/googlemap.js
@@ -1,9 +1,11 @@
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: (give the lan), lng: (give lon)},
-        zoom: 8
-    });
+    var map = new google.maps.Map(document.getElementById("map-canvas"), {
+        center: new google.maps.LatLng(7.8731, 80.7718),
+        zoom: 8,
+        minZoom:8,
+        mapTypeId: 'roadmap'
+      });
 
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhSKzfElSK1IBSQgF1kGr2Iv6-JqeVUUA &callback=initMap"
 }
