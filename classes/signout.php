2b81e26b0e0dbb27c6318618f041b60213655bfd
dineth454 <dineth454@gmail.com>
2016-04-20 13:08:00 +0530

    completed login 80%

--- classes/signout.php
+++ classes/signout.php
@@ -1,11 +1,9 @@
-<?php 
+<?php
 	session_start();
 	session_unset();
 	session_destroy();
-	ob_start();
-	header("location:../SystemLogin.php");
-	ob_end_flush(); 
-	include '../SystemLogin.php';
-	//include 'home.php';
+	header("location: ../interfaces/SystemLogin.php");
 	exit();
+
 ?>
+
--- interfaces/navigationBar.php
+++ interfaces/navigationBar.php
@@ -1,7 +1,23 @@
 
+<?php
+session_start();
+ob_start();
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+//header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
+if (!isset($_SESSION['fullName']) || time() - $_SESSION['login_time'] > 300) {
+    header('Location: SystemLogin.php');
+    exit();
+}
+else{
+	$_SESSION['login_time'] = time();
+}
+ob_end_flush();
+?>
+
 <nav class="navbar navbar-default" style="height: 65px; border-radius:0px;">
                 <div class="col-md-3 pull-right" style="margin-top: 18px;">
-                    Nipuna Jayaweera 
+                    <label><?php echo $_SESSION["fullName"]?></label>
                     <div class="pull-right"><a href="../classes/signout.php">Sign out</a></div>
 
                 </div>
