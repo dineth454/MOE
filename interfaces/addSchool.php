c479898bbbe749beff65c27f58d25bfea2826e27
dineth454 <dineth454@gmail.com>
2016-04-24 23:44:53 +0530

    correction of addSchool.php

--- interfaces/addSchool.php
+++ interfaces/addSchool.php
@@ -26,9 +26,6 @@ ob_start();
         <link href="../assets/css/navbar_styles.css" rel="stylesheet">
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhSKzfElSK1IBSQgF1kGr2Iv6-JqeVUUA"></script>
 
-        
-
-
     </head>
 
     <body onload="initialize()">
@@ -57,7 +54,7 @@ ob_start();
                             $provinceId = $_POST['provinceID'];
                             $zonalId = $_POST['zonalID'];
                             $school = $_POST['School'];
-                            $SchoolType = $_POST['SchoolType'];
+                            $SchoolType = $_POST['SchoolTypeSelect'];
                             $NoOfStudents = $_POST['students'];
                             $lat = $_POST['lat'];
                             $lang = $_POST['lng'];
@@ -66,13 +63,8 @@ ob_start();
 
 
                             $insertSuccess = $institute->addschool($provinceId, $zonalId, $school,$SchoolType,$NoOfStudents,$lat,$lang);
-
                             
                         }
-
-                        
-
-
                     ?>
 
                         <div align="center" style="padding-bottom:10px;">
