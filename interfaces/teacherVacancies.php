d0ef8322de51e19d5939513a81ca02a27a81a7f6
KalingaYapa <kkyapa@gmail.com>
2016-04-27 10:39:08 +0530

    TeacherVAncies.php and sideBarPrincipal.php

---
+++ interfaces/teacherVacancies.php
@@ -0,0 +1,109 @@
+<?php
+ob_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Vacancies Teacher Form</title>
+
+
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../assets/css/simple-sidebar.css" rel="stylesheet">
+        <link href="../assets/css/home.css" rel="stylesheet">
+        <link href="../assets/css/smallbox.css" rel="stylesheet">
+        <link href="../assets/css/footer.css" rel="stylesheet">
+        <link href="../assets/css/navbar_styles.css" rel="stylesheet">
+        <!--<link href="../assets/css/fonts_styles.css" rel="stylesheet">-->
+
+
+    </head>
+
+    <body>
+
+        <div id="wrapper" > 
+
+            <!-- Sidebar -->
+            <?php include 'sideBarPrincipal.php' ?>
+            <!-- /#sidebar-wrapper -->
+
+            <!-- include Navigation BAr -->
+            <?php include 'navigationBar.php' ?>
+
+            <!-- Finished NAvigation bar -->
+            <!-- Page Content -->
+            <div id="page-content-wrapper" style="min-height: 540px;">
+
+                <div class="container-fluid">
+                    <div class="col-lg-9 col-lg-offset-1">
+
+
+
+
+                        <div align="center" style="padding-bottom:10px;">
+                            <h1>Vacancies</h1>
+                        </div>
+
+                        <form action="" method = "post"  onsubmit="" novalidate>
+
+                            <div class="row">
+                                <div class="form-group col-lg-12 col-md-12 col-sm-12">
+
+                                    <div class="row">
+                                        <div class="form-group col-lg-12 col-md-12 col-sm-12">
+
+                                            <!-- NIC number-->
+                                            <label  for="nic" class="control-label col-xs-6 col-sm-3 col-md-3 col-lg-3" style="display: inline-block; text-align: left;">Enter NIC Number :</label>
+                                            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+                                                <input type="text"  class="form-control" id="nic" name="nic" placeholder="Enter NIC number"/>
+                                                <label id="errorNicNumbertranser" style="font-size:10px"> </label>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                                            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+                                                <button type="submit" name="submit" id="submit" class="btn btn-primary">Find</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+
+
+                        </form>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+            <!-- /#page-content-wrapper -->
+
+
+
+
+            <?php include 'footer.php' ?>
+            <script src="../assets/js/jquery.js"></script>
+            <script src="../assets/js/bootstrap.min.js"></script>
+            <script src = "../assets/js/jquery-2.1.4.min.js"></script>
+
+
+
+        </div>
+    </body>
+
+</html>
