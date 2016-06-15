83798df9755c0b4a1dad6b1959860ced2f6c6ebf
Pasii <pasindudeeyagahage@gmail.com>
2016-06-16 00:08:42 +0530

    sidebar problem fixed1

---
+++ interfaces/sidebar_min_off.php
@@ -0,0 +1,80 @@
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="interface_0.1.php"><i class="fa fa-home fa-2x" aria-hidden="true"></i> Home</a>
+                    </li>
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#collapse1"><i class="fa fa-users fa-2x" aria-hidden="true"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="collapse1" class="collapse">
+                            <li>
+                                <a href="addEmployee.php">Add User</a>
+                            </li>
+                            <li>
+                                <a href="updateEmployeeFront.php">Update User</a>
+                            </li>
+                            <li>
+                                <a href="deleteUserFront.php">Delete User</a>
+                            </li>
+                        </ul>
+                    </li>
+                   
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#collapse2"><i class="fa fa-university fa-2x" aria-hidden="true"></i> Institutes <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="collapse2" class="collapse">
+                            <li>
+                                <a href="addZonalOffice.php">Add Zonal</a>
+                            </li>
+                            <li>
+                                <a href="addSchool.php">Add School</a>
+                            </li>
+                            <li>
+                                <a href="updateSchool.php">Update School</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#collapse3"><i class="fa fa-book fa-2x" aria-hidden="true"></i> Subject <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="collapse3" class="collapse">
+                            <li>
+                                <a href="addNewSubject.php">Add Subject</a>
+                            </li>
+                            <li>
+                                <a href="addCurrentSubjectFormFront.php">Add Current subject Of Teacher</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#collapse4"><i class="fa fa-search fa-2x" aria-hidden="true"></i> Search <i class="fa fa-fw fa-caret-down"></i></a>
+                        
+                    </li>
+
+
+                    <li>
+                        <a href="javascript:;" data-toggle="collapse" data-target="#collapse5"><i class="fa fa-exchange fa-2x" aria-hidden="true"></i> Transfer <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="collapse5" class="collapse">
+                            <li>
+                                <a href="addNewSubject.php">Transfer Teacher</a>
+                            </li>
+                            
+                            
+                        </ul>
+                    </li>
+
+        
+       
+       
+                    <li>
+                        <a href="mapview.php" class="ajax_load slide_font">Map View</a>
+                    </li>
+
+                </ul>
+</div>
+
+
+
+
+                   
+             
