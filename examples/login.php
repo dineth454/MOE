f663cb6cd1d6963b2fef4f11872c8972474ad668
KalingaYapa <kkyapa@gmail.com>
2016-06-25 22:41:16 +0530

    Reports generation

---
+++ examples/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+<?php 
+	/*if (isset($_POST['submit'])) {
+		$userName = $_POST['username'];
+		$password = $_POST['password'];
+
+	}*/
+?>
+	<h1>Login</h1>
+	<form method="POST" action="pdf.php"  >
+		<input type="text" name ="username">UserNAme</input>
+		<input type="password" name ="password">Password</input>
+		<button type="submit" name= "submit">submit</button>
+	</form>
+	
+
+
+</script>
+</body>
+
