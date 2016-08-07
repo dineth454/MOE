0826748b7c84206af623d1d99dea646b1db9bc53
nipuna_ <nipuna.player@gmail.com>
2016-08-07 17:27:46 +0530

    search vacancy

---
+++ interfaces/viewVacancy.php
@@ -0,0 +1,139 @@
+<?php
+ob_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Home</title>
+
+        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="../assets/css/simple-sidebar.css" rel="stylesheet">
+        <link href="../assets/css/home.css" rel="stylesheet">
+        <link href="../assets/css/smallbox.css" rel="stylesheet">
+        <link href="../assets/css/footer.css" rel="stylesheet">
+        <link href="../assets/css/fonts_styles.css" rel="stylesheet">
+        <link href="../assets/css/navbar_styles.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <div id="wrapper">
+
+            <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color:#020816;" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <!-- include Navigation BAr -->
+            <?php include 'navigation_bar.php' ?>
+            <!--____________________________________________________________________________-->
+            <!-- Sidebar Menu Items-->
+             <!-- Sidebar -->
+            
+            <!-- /#sidebar-wrapper -->
+            <!-- /.navbar-collapse -->
+            </nav>
+
+
+
+            <div  id="page-content-wrapper" style="min-height: 540px;" >
+
+                <div class="container-fluid">
+                    <div class="col-lg-9 col-lg-offset-1">
+                        <?php 
+
+                        require("../classes/vacansies.php");
+                        $vacancy = new Vacancy();
+                        $provinceID = "n";
+                        if (isset($_POST['submit'])) {
+
+                            $provinceID = $_POST['provinceID'];
+                        }
+
+                        
+
+
+
+                        ?>
+                        
+                        <div align="center" style="padding-bottom:10px;">
+                            <h1 class="topic_font">View vacancies</h1>
+                        </div>
+
+                        <form name="addEmployeeForm" method = "post" onsubmit="return(validateForm())"  novalidate>
+
+                            <div class="row">
+                                <div class="form-group col-lg-12 col-md-12 col-sm-12">
+
+                                    <div class="row">
+                                        <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                                            <!-- Designation-->
+                                            <label for="designation" class="control-label col-xs-6 col-sm-3 col-md-3 col-lg-3 required" style="display: inline-block; text-align: left;"> Province </label>
+                                            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3"  >
+                                                    <select required class="form-control " name="provinceID" id="provinceID" onchange="showUser(this.value)">
+                                                        <option value="" >Select ProvinceOffice</option>
+                                                        <option value="1">centralProvince</option>
+                                                        <option value="2">westernProvince</option>
+                                                        <option value="3">sothernProvince</option>
+                                                        <option value="4">NothernProvince</option>
+                                                        <option value="5">esternProvince</option>
+                                                    </select>
+                                                    
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                                            <div class="col-xs-6 col-sm-3 col-md-3 col-lg-3">
+                                                <button type="submit" name="submit" id="submit" class="btn btn-primary">Submit</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-lg-12 col-md-12 col-sm-12">
+                                            
+                                                <?php echo $vacancy-> viewVacancy($provinceID) ; ?>
+
+                                        </div>
+                                    </div>
+
+
+                                </div>
+
+                            </div>
+
+                        </form>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+            <!-- /#page-content-wrapper -->
+
+        </div>
+
+        <?php include 'footer.php' ?>
+        <script src = "../assets/js/addEmployee.js"></script>
+
+        <script src="../assets/js/jquery.js"></script>
+
+
+        <script src="../assets/js/bootstrap.min.js"></script>
+        <script src = "../assets/js/jquery-2.1.4.min.js"></script>
+
+
+
+
+
+    </body>
+
+</html>
