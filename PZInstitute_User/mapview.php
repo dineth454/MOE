2973ccf68a443d625c91467036fe91550d77d5bb
dineth454 <dineth454@gmail.com>
2016-08-29 23:36:49 +0530

    edited PZUser add employee

--- PZInstitute_User/mapview.php
+++ PZInstitute_User/mapview.php
@@ -76,7 +76,7 @@ ob_start();
             <div id="page-content-wrapper" style="min-height: 550px;">
 
                 <div class="container-fluid" >
-                    <div id="map-canvas" style="width:500px;height:730px;"></div>
+                    <div id="map-canvas" style="width:500px;height:730px; box-shadow: 0px 0px 15px #595959;"></div>
 
 
 
